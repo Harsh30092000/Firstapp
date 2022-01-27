9bb018a7950f2932295c79b9a508f2d88a2293b6
Harsh30092000 <harshith30092000@gmail.com>
2022-01-27 15:42:05 +0530

    New commit

--- app/src/main/java/com/example/firstapp/HomeActivity.kt
+++ app/src/main/java/com/example/firstapp/HomeActivity.kt
@@ -4,17 +4,31 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 
 class HomeActivity : AppCompatActivity() {
+    lateinit var monthView:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        var month = intent.extras?.getString("Jan")
+        var monthView = findViewById<TextView>(R.id.month_view)
+        monthView.text = month
     }
 
     fun makeAnotherToast(view: View) {
         Toast.makeText(this,"You Have come to the right Place",Toast.LENGTH_LONG).show()
         var intent = Intent(this,MainActivity::class.java)
+        //intent.putExtra("ds","divyang sharma")
         startActivity(intent)
+        startActivityForResult(intent,123)
+    }
+    //result will arrive here -- photo, contact
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == RESULT_OK && requestCode == 123){ //RESULT_OK means its good to consume, 123 -- data being returned is of type contact
+            monthView.text = data?.extras?.getString("con")
+        }
     }
 }
--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.firstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,18 +17,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        /*
+        String name = getIntent().getExtras().getString("ds");
+        TextView mTextView = findViewById(R.id.tvMain);
+        mTextView.setText(name);
+         */
+    }
+    private void sendContact() {
+        //get the contact from edittext
+        EditText nameEditText = findViewById(R.id.name);
+        String contact = nameEditText.getText().toString();
+        //put the contact in intent
+        Intent cIntent = new Intent();
+        cIntent.putExtra("con",contact);
+        //set the result
+        setResult(RESULT_OK,cIntent);
+        //close this activity
+        finish();
+    }
     public void makeToast(View view) {
-        Log.i(TAG,"makong toast");
+        Log.i(TAG,"making toast");
         Toast.makeText(this, "Logging You in", Toast.LENGTH_SHORT).show();
         //go to different activity
         Intent myIntent = new Intent(this,HomeActivity.class);
+        myIntent.putExtra("Jan","January");
         startActivity(myIntent);
     }
 
     public void handleClick(View view) {
         Log.i(TAG,"handling  toast");
         Toast.makeText(this, "clicked", Toast.LENGTH_SHORT).show();
+        Intent dialIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("917867562"));
+        startActivity(dialIntent);
     }
+
 }
