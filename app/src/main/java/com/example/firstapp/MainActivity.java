d06b1cfd83415bfc8f54df9abcd94505bac983c2
Harsh30092000 <harshith30092000@gmail.com>
2022-01-27 10:26:13 +0530

    Initial commit

---
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.firstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    public static String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void makeToast(View view) {
+        Log.i(TAG,"makong toast");
+        Toast.makeText(this, "Logging You in", Toast.LENGTH_SHORT).show();
+    }
+
+    public void handleClick(View view) {
+        Log.i(TAG,"handling  toast");
+
+        Toast.makeText(this, "clicked", Toast.LENGTH_SHORT).show();
+    }
+}
