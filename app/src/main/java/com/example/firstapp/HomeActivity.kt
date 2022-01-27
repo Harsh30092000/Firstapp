c683058f22ddab8c593341689f0144b3bc1166e1
Harsh30092000 <harshith30092000@gmail.com>
2022-01-27 11:32:41 +0530

    Added Kotlin Files

---
+++ app/src/main/java/com/example/firstapp/HomeActivity.kt
@@ -0,0 +1,17 @@
+package com.example.firstapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+
+    fun makeAnotherToast(view: View) {
+        Toast.makeText(this,"You Have come to the right Place",Toast.LENGTH_LONG).show()
+    }
+}
